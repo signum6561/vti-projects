7dc92c3b53e7e015463d20116ff7e36e621a6a02
signum6561 <quiocdat777@gmail.com>
2024-03-25 00:04:27 +0700

    update final project

--- final project/main.js
+++ final project/main.js
@@ -33,8 +33,8 @@ const fetchUsers = async () => {
   return res.json();
 };
 
-const fetchUserById = async (productId) => {
-  const res = await fetch(`${BASE_API}/users/${productId}`);
+const fetchUserById = async (userId) => {
+  const res = await fetch(`${BASE_API}/users/${userId}`);
   return res.json();
 };
 
@@ -52,17 +52,18 @@ const initializeProductPage = () => {
         <input 
           type="text" 
           placeholder="Search by product name..." 
-          oninput="setProductNameFilter(this.value)">
+          oninput="setFilter('searchData', this.value)">
       </div>
       <div class="sort-container">
-        <select class="regular-btn-type sort-btn" id="sort-option" onchange="setProductPriceOrderFilter(this.value)">
+        <select class="regular-btn-type sort-btn" id="sort-option" onchange="setFilter('order',this.value)">
+          <option value="" disabled selected hidden>Sort by</option>
           <option value="">None</option>
           <option value="asc">Price Asc</option>
           <option value="desc">Price Desc</option>
         </select>
       </div>
-      <div class="isUsed-checkbox">
-        <input id="isUsed" type="checkbox" onclick="setProductIsUsedFilter(this.checked)">
+      <div class="input-checkBox">
+        <input id="isUsed" type="checkbox" onclick="setFilter('isUsed', this.checked)">
         <span>is used</span>
       </div>
     </div>
@@ -100,8 +101,8 @@ const updateProduct = async (updatedProduct) => {
   return res.ok;
 };
 
-const deleteProduct = async (id) => {
-  const res = await fetch(`${BASE_API}/products/${id}`, {
+const deleteProduct = async (productId) => {
+  const res = await fetch(`${BASE_API}/products/${productId}`, {
     method: 'DELETE',
   });
   return res.ok;
@@ -111,7 +112,7 @@ const handleGetAllProduct = async () => {
   contentDiv.innerHTML = loadingScreen();
   const productList = await fetchProducts();
   if (productList.length > 0) {
-    sessionStorage.setItem('product-list', JSON.stringify(productList));
+    sessionStorage.setItem('temp-list', JSON.stringify(productList));
     contentDiv.innerHTML = generateProductCardList(productList);
   }
 };
@@ -206,15 +207,16 @@ const handleDeleteProduct = async (productId) => {
   }
 };
 
-const filterProductList = (filters) => {
-  const productList = sessionStorage.getItem('product-list');
+const handleFilterProducts = (filters) => {
+  const productList = sessionStorage.getItem('temp-list');
   let newProductList = JSON.parse(productList);
   if (filters.isUsed) {
     newProductList = newProductList.filter((product) => product.isUsed);
   }
-  if (filters.name) {
+  if (filters.searchData) {
+    const _searchData = filters.searchData.toLowerCase();
     newProductList = newProductList.filter((product) =>
-      product.name.toLowerCase().includes(filters.name),
+      product.name.toLowerCase().includes(_searchData),
     );
   }
   if (filters.order === 'asc') {
@@ -228,19 +230,9 @@ const filterProductList = (filters) => {
       : noResultScreen();
 };
 
-const setProductIsUsedFilter = (isEnable) => {
-  currentFilters.isUsed = isEnable;
-  filterProductList(currentFilters);
-};
-
-const setProductPriceOrderFilter = (order) => {
-  currentFilters.order = order;
-  filterProductList(currentFilters);
-};
-
-const setProductNameFilter = (name) => {
-  currentFilters.name = name.toLowerCase();
-  filterProductList(currentFilters);
+const setFilter = (filterName, value) => {
+  currentFilters[filterName] = value;
+  handleFilterProducts(currentFilters);
 };
 
 const generateProductCardList = (productList) => {
@@ -367,9 +359,9 @@ const initializeUserPage = () => {
       <input 
         type="text" 
         placeholder="Search by user name..." 
-        oninput="filterProductByName(this.value)">
+        oninput="filterUserByName(this.value)">
     </div>
-    <button class="regular-btn-type add-btn" onclick="displayCreateProductForm()">
+    <button class="regular-btn-type add-btn" onclick="displayCreateUserForm()">
       <span 
         class="iconify" 
         data-icon="mingcute:add-fill"
@@ -403,8 +395,8 @@ const updateUser = async (updatedUser) => {
   return res.ok;
 };
 
-const deleteUser = async (id) => {
-  const res = await fetch(`${BASE_API}/users/${id}`, {
+const deleteUser = async (userId) => {
+  const res = await fetch(`${BASE_API}/users/${userId}`, {
     method: 'DELETE',
   });
   return res.ok;
@@ -414,9 +406,237 @@ const handleGetAllUser = async () => {
   contentDiv.innerHTML = loadingScreen();
   const userList = await fetchUsers();
   if (userList.length > 0) {
-    sessionStorage.setItem('user-list', JSON.stringify(userList));
+    sessionStorage.setItem('temp-list', JSON.stringify(userList));
     contentDiv.innerHTML = generateUserCardList(userList);
   }
+};
+
+const displayCreateUserForm = () => {
+  createTitle.innerHTML = `Create User`;
+  createForm.innerHTML = generateCreateUserForm();
+  createForm.addEventListener('submit', (event) => handleAddUser(event));
+  handleCreateModal(1);
+};
+
+const displayUpdateUserForm = (user) => {
+  editTitle.innerHTML = `Edit User`;
+  editForm.innerHTML = generateEditUserForm();
+  editForm.addEventListener('submit', (event) => handleUpdateUser(event));
+  document.getElementById('edit-user-name').value = user.name;
+  document.getElementById('edit-user-gender').value = user.gender;
+  document.getElementById('edit-user-job').value = user.job;
+  document.getElementById('edit-user-dateBirth').value = user.dateBirth.slice(
+    0,
+    10,
+  );
+  document.getElementById('edit-user-bornAt').value = user.bornAt;
+  document.getElementById('edit-user-avatar').value = user.avatar;
+  sessionStorage.setItem('selected-user-id', user.id);
+  handleEditModal(1);
+};
+
+const handleAddUser = async (event) => {
+  event.preventDefault();
+  handleCreateModal(0);
+  const name = document.getElementById('create-user-name').value;
+  const gender = document.getElementById('create-user-gender').value;
+  const job = document.getElementById('create-user-job').value;
+  const dateBirth = document.getElementById('create-user-dateBirth').value;
+  const bornAt = document.getElementById('create-user-bornAt').value;
+  const avatar = document.getElementById('create-user-avatar').value;
+  const createdDate = new Date().toJSON();
+  const newUser = {
+    name,
+    gender,
+    job,
+    dateBirth,
+    bornAt,
+    avatar,
+    createdDate,
+  };
+
+  const isCreated = await createUser(newUser);
+  if (isCreated) {
+    openSuccessMsg();
+    handleGetAllUser();
+  } else {
+    openErrorMsg();
+  }
+  handleCreateModal(0);
+};
+
+const handleUpdateUser = async (event) => {
+  event.preventDefault();
+  handleEditModal(0);
+  const name = document.getElementById('edit-user-name').value;
+  const gender = document.getElementById('edit-user-gender').value;
+  const job = document.getElementById('edit-user-job').value;
+  const dateBirth = document.getElementById('edit-user-dateBirth').value;
+  const bornAt = document.getElementById('edit-user-bornAt').value;
+  const avatar = document.getElementById('edit-user-avatar').value;
+  const updatedUser = {
+    id: sessionStorage.getItem('selected-user-id'),
+    name,
+    gender,
+    job,
+    dateBirth,
+    bornAt,
+    avatar,
+  };
+
+  const isUpdated = await updateUser(updatedUser);
+  if (isUpdated) {
+    openSuccessMsg();
+    handleGetAllUser();
+  } else {
+    openErrorMsg();
+  }
+  handleEditModal(0);
+};
+
+const handleDeleteUser = async (userId) => {
+  try {
+    await deleteUser(userId);
+    openSuccessMsg();
+    handleGetAllUser();
+  } catch (err) {
+    console.log(err);
+    openErrorMsg();
+  }
+};
+
+const filterUserByName = (userName) => {
+  const searchData = userName.toLowerCase();
+  const userList = sessionStorage.getItem('temp-list');
+  const filteredList = JSON.parse(userList).filter((user) =>
+    user.name.toLowerCase().includes(searchData),
+  );
+  contentDiv.innerHTML =
+    filteredList.length > 0
+      ? generateUserCardList(filteredList)
+      : noResultScreen();
+};
+
+const generateUserCardList = (userList) => {
+  return `
+    <div class="grid-container">
+      ${userList.map((user) => generateUserCard(user)).join('')}
+    </div>
+    `;
+};
+
+const generateUserCard = (user) => {
+  return `
+    <div class="card img-info">
+      <img class="card-img" src="${user.avatar}" alt="">
+      <div class="base-info">
+        <p><strong>ID:</strong> ${user.id}</p>
+        <p><strong>Name:</strong> ${user.name}</p>
+        <p><strong>Gender:</strong> ${user.gender}</p>
+      </div>
+      <div class="btn-container">
+        <span
+          class="circle-btn-type viewDetail-btn iconify"
+          onclick="viewDetailUser(${user.id})"
+          data-icon="gg:details-more"
+        ></span>
+        <span
+          class="circle-btn-type edit-btn iconify"
+          onclick='displayUpdateUserForm(${JSON.stringify(user)})'
+          data-icon="lucide:edit"
+        ></span>
+        <span
+          class="circle-btn-type delete-btn iconify"
+          onclick="handleDeleteUser(${user.id})"
+          data-icon="ion:trash-outline"
+        ></span>
+      </div>
+    </div>
+    `;
+};
+
+const generateCreateUserForm = () => {
+  return `
+    <div class="input-textBox">
+      <label for="name">Name</label
+      ><input id="create-user-name" type="text" required />
+    </div>
+    <div class="input-selectBox">
+      <label for="gender">Gender</label
+      ><select id="create-user-gender" required>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+      </select>
+    </div>
+    <div class="input-textBox">
+      <label for="job">Job</label
+      ><input id="create-user-job" type="text" required />
+    </div>
+    <div class="input-dateBox">
+      <label for="dateBirth">Date of Birth</label
+      ><input id="create-user-dateBirth" type="date" required />
+    </div>
+    <div class="input-textBox">
+      <label for="bornAt">Born At</label
+      ><input id="create-user-bornAt" type="text" required />
+    </div>
+    <div class="input-textBox">
+      <label for="avatar">Avatar</label
+      ><input id="create-user-avatar" type="text" required />
+    </div>
+  `;
+};
+
+const generateEditUserForm = () => {
+  return `
+    <div class="input-textBox">
+      <label for="name">Name</label
+      ><input id="edit-user-name" type="text" required />
+    </div>
+    <div class="input-selectBox">
+      <label for="gender">Gender</label
+      ><select id="edit-user-gender" required>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+      </select>
+    </div>
+    <div class="input-textBox">
+      <label for="job">Job</label
+      ><input id="edit-user-job" type="text" required />
+    </div>
+    <div class="input-dateBox">
+      <label for="dateBirth">Date of Birth</label
+      ><input id="edit-user-dateBirth" type="date" required />
+    </div>
+    <div class="input-textBox">
+      <label for="bornAt">Born At</label
+      ><input id="edit-user-bornAt" type="text" required />
+    </div>
+    <div class="input-textBox">
+      <label for="avatar">Avatar</label
+      ><input id="edit-user-avatar" type="text" required />
+    </div>
+  `;
+};
+
+const viewDetailUser = async (userId) => {
+  const user = await fetchUserById(userId);
+  detailTitle.innerHTML = `User Detail Title`;
+  detailContent.innerHTML = `
+    <div class="img-info">
+      <img class="detail-img" src="${user.avatar}" alt="">
+      <div class="base-info">
+        <p><strong>ID:</strong> ${user.id}</p>
+        <p><strong>Name:</strong> ${user.name}</p>
+        <p><strong>Gender:</strong> ${user.gender}</p>
+        <p><strong>Job:</strong> ${user.job}</p>
+        <p><strong>Born At:</strong> ${user.bornAt}</p>
+        <p><strong>Date of Birth:</strong> ${user.dateBirth.slice(0, 10)}</p>
+        <p><strong>Created:</strong> ${user.createdDate}</p>
+      </div>
+    </div>
+  `;
+  handleDetailModal(1);
 };
 
 //#endregion
